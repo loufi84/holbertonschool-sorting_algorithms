3164a60c2922b17a3a94b56a5c97b4fd5c2c8969
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-07 20:32:27 +0200

    Updated header + created source files for print_array, print_list and answer to task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function that sorts an array of int in
+ *				ascending order (bubble sort)
+ *
+ * @array: The array to sort
+ * @size: Number of elements
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t tmp, i = 0, j = 0;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}

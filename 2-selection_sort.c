fd0bbccd882021cd2b98c7b065816aeba05fd543
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-08 10:44:20 +0200

    Updated header and 2.c and created source files for task 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,9 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min, tmp;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
---
+++ 3-quick_sort.c
@@ -0,0 +1,93 @@
+#include "sort.h"
+
+/**
+ * swap - A function that swap 2 integers
+ *
+ * @a: First integer
+ * @b: Second integer
+ */
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	if (a != b)
+	{
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
+}
+
+/**
+ * lomuto - A function that finds the pivot point
+ *
+ * @array: The array to treat
+ * @size:The size of the array
+ * @low: The low point
+ * @high: The high point
+ *
+ * Return: The Lomuto point
+ */
+
+int lomuto(int *array, size_t size, int low, int high)
+{
+	int pivot = array[high];
+	int i = low, j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[high], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != high)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quicksort_rec - A function to sort recursively an array of int
+ *
+ * @array: A pointer to the first element of the array
+ * @size: The size of the array
+ * @low: The low point
+ * @high: The high point
+ */
+
+void quicksort_rec(int *array, size_t size, int low, int high)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto(array, size, low, high);
+		quicksort_rec(array, size, low, pivot_index - 1);
+		quicksort_rec(array, size, pivot_index + 1, high);
+	}
+}
+
+/**
+ * quick_sort - A function that sorts an array of integers
+ *				in ascending order usig the quick sort algorithm
+ *
+ * @array: A ponter to the first element of the array to sort
+ * @size: The size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quicksort_rec(array, size, 0, size - 1);
+}

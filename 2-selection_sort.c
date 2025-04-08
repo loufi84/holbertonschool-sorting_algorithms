f6a99e8d5ffabcd166e32bd840afa4806f8e9d05
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-08 10:03:31 +0200

    Updated header + created source files for task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - A function that sorts an array of integers in ascending
+ *					order using the selection sort algorithm
+ *
+ * @array: A pointer to the first element of the array to sort
+ * @size: The size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+	}
+}
